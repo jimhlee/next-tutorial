edfb766d0603ec01f69e4698175e016959ef42c8
James Lee <jimhlee23@gmail.com>
2024-08-05 14:53:51 -0400

    refator auth

--- src/auth.ts
+++ src/auth.ts
@@ -1,7 +1,11 @@
 import NextAuth from "next-auth"
-import GitHub from "next-auth/providers/github"
-import Google from "next-auth/providers/google"
+import { PrismaAdapter } from "@auth/prisma-adapter"
+import authConfig from "./auth.config"
+import { prisma } from "./lib/prisma";
 
-export const { auth, handlers, signIn, signOut } = NextAuth({
-  providers: [GitHub, Google],
+
+export const { auth, handlers: {GET, POST}, signIn, signOut } = NextAuth({
+  adapter: PrismaAdapter(prisma),
+  session: { strategy: "jwt" },
+  ...authConfig,
 })
